1b55845c4b398a191dc0a8ecd9468ec222851456
yaminisrujana04 <yaminisrujanaa@gmail.com>
2025-01-07 10:50:03 +0530

    sent

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("javascript is linked to html...")
+document.writeln("Javscript linked");
+let str1 = "double quotes";
+console.log(str1);
+console.log(typeof(str1));
+
+let str2 = 'single quotes';
+console.log(str2);
+console.log(typeof(str2));
+let str3 =" template literals";
+console.log(str3);
+console.log(typeof(str3));
+
+let num = 10.345678;
+console.log(num , typeof(num));
+
+let bool = true;
+console.log(bool , typeof(bool));
+
+let a;
+console.log(a , typeof(a));
+
+let b = null;
+console.log(b , typeof(b));
