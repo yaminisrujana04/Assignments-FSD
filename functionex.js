06407dd71d421b53ba0ff890920cbfcb26017331
yaminisrujana04 <yaminisrujanaa@gmail.com>
2025-01-08 11:54:39 +0530

    pushed

---
+++ functionex.js
@@ -0,0 +1,13 @@
+// !without parameters and arguments
+// function demo()// functionn declareation
+// {
+//     console.log("hello")
+// }
+// demo()//function calling
+
+//? with paramaters and arguments
+function demo1(a,b){
+    console.log(a+b);
+}
+demo1(5,6);
+demo1(11,22);
---
+++ loopingstatements.js
@@ -0,0 +1,20 @@
+// //factorial of 5
+// let n = 5;
+// let result = 1;
+// for (let i = 1; i <= n; i++) {
+//  result *= i;
+// }
+//     console.log(result);
+
+    // while loop
+//  let i=1
+//  while(i <= 5 ){
+//     console.log("good morning")
+//  }
+
+//do while
+let i=1
+do{
+   console.log("wakeup")
+   i++;
+}while(i<=5);
